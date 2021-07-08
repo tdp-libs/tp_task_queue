f33763df394294c7779102e958e58d9ee8024d63
Tom Paynter <tompaynter@tdpe.co.uk>
2021-07-08 13:18:16 +0100

    Fixed the OBJ import, added a plane to material import, lots of linting.

--- inc/tp_task_queue/WorkQueue.h
+++ inc/tp_task_queue/WorkQueue.h
@@ -12,6 +12,7 @@ namespace tp_task_queue
 //! Similar to the TaskQueue but far more simple and guarantees to complete all task on destruction.
 class TP_TASK_QUEUE_SHARED_EXPORT WorkQueue
 {
+  TP_NONCOPYABLE(WorkQueue);
 public:
   //################################################################################################
   WorkQueue(const std::string& threadName);
