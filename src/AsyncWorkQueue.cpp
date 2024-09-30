2154d625f079bcbb7a455d3546c7674dbee04239
Tom Paynter <tompaynter@tdpe.co.uk>
2024-09-30 16:18:54 +0100

    Improvements to catalogs and added render interruption.

--- src/AsyncWorkQueue.cpp
+++ src/AsyncWorkQueue.cpp
@@ -1,6 +1,7 @@
 #include "tp_task_queue/AsyncWorkQueue.h"
 
 #include "tp_utils/Progress.h"
+#include "tp_utils/TPCheckMainThread.h"
 
 namespace tp_task_queue
 {
@@ -8,6 +9,9 @@ namespace tp_task_queue
 //##################################################################################################
 struct AsyncWorkQueue::Private
 {
+  TPCheckMainThread checkMainThread;
+
+  tp_utils::Progress* progress{nullptr};
   std::function<bool()> poll;
 
   bool ok{true};
@@ -16,8 +20,17 @@ struct AsyncWorkQueue::Private
 
   std::vector<std::function<RunAgain(AsyncWorkQueue*)>*> tasks;
 
+  //################################################################################################
   Private(const std::function<bool()>& poll_):
     poll(poll_)
+  {
+
+  }
+
+  //################################################################################################
+  Private(tp_utils::Progress* progress_):
+    progress(progress_),
+    poll([&]{return progress->poll();})
   {
 
   }
@@ -31,8 +44,16 @@ AsyncWorkQueue::AsyncWorkQueue(const std::function<bool()>& poll):
 }
 
 //##################################################################################################
+AsyncWorkQueue::AsyncWorkQueue(tp_utils::Progress* progress):
+  d(new Private(progress))
+{
+
+}
+
+//##################################################################################################
 AsyncWorkQueue::~AsyncWorkQueue()
 {
+  d->checkMainThread("AsyncWorkQueue::~AsyncWorkQueue");
   tpDeleteAll(d->tasks);
   delete d;
 }
@@ -40,18 +61,29 @@ AsyncWorkQueue::~AsyncWorkQueue()
 //##################################################################################################
 void AsyncWorkQueue::addTask(const std::function<RunAgain(AsyncWorkQueue*)>& task)
 {
+  d->checkMainThread("AsyncWorkQueue::addTask");
   d->tasks.emplace_back(new std::function<RunAgain(AsyncWorkQueue*)>(task));
 }
 
 //##################################################################################################
 const std::function<bool()>& AsyncWorkQueue::poll() const
 {
+  d->checkMainThread("AsyncWorkQueue::poll");
   return d->poll;
+}
+
+//##################################################################################################
+tp_utils::Progress* AsyncWorkQueue::progress() const
+{
+  d->checkMainThread("AsyncWorkQueue::poll");
+  return d->progress;
 }
 
 //##################################################################################################
 bool AsyncWorkQueue::wait()
 {
+  d->checkMainThread("AsyncWorkQueue::wait");
+
   size_t nextIndex=0;
   while(d->poll() && !d->tasks.empty())
   {
@@ -71,6 +103,7 @@ bool AsyncWorkQueue::wait()
 //##################################################################################################
 void AsyncWorkQueue::setOk(bool ok)
 {
+  d->checkMainThread("AsyncWorkQueue::setOk");
   d->ok = ok;
   changed();
 }
@@ -78,6 +111,7 @@ void AsyncWorkQueue::setOk(bool ok)
 //##################################################################################################
 bool AsyncWorkQueue::ok() const
 {
+  d->checkMainThread("AsyncWorkQueue::ok");
   return d->ok;
 }
 
@@ -85,26 +119,38 @@ bool AsyncWorkQueue::ok() const
 //##################################################################################################
 void AsyncWorkQueue::setShouldStop(bool shouldStop)
 {
+  d->checkMainThread("AsyncWorkQueue::setShouldStop");
   d->shouldStop = shouldStop;
 }
 
 //##################################################################################################
 bool AsyncWorkQueue::shouldStop() const
 {
+  d->checkMainThread("AsyncWorkQueue::shouldStop");
   return d->shouldStop;
 }
 
 //##################################################################################################
-void AsyncWorkQueue::addError(const std::string& message)
+void AsyncWorkQueue::addError(const std::string& error)
 {
+  d->checkMainThread("AsyncWorkQueue::addError");
+
+  if(d->progress)
+    d->progress->addError(error);
+
   d->ok = false;
-  d->messages.emplace_back(message, true, 0);
+  d->messages.emplace_back(error, true, 0);
   changed();
 }
 
 //##################################################################################################
 void AsyncWorkQueue::addMessage(const std::string& message)
 {
+  d->checkMainThread("AsyncWorkQueue::addMessage");
+
+  if(d->progress)
+    d->progress->addMessage(message);
+
   d->messages.emplace_back(message, false, 0);
   changed();
 }
@@ -112,6 +158,7 @@ void AsyncWorkQueue::addMessage(const std::string& message)
 //##################################################################################################
 const std::vector<tp_utils::ProgressMessage>& AsyncWorkQueue::messages() const
 {
+  d->checkMainThread("AsyncWorkQueue::messages");
   return d->messages;
 }
 
